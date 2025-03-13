74b0bb92b8550ffb54a6caeabed8f5203b312f06
hurui <hu.rui0530@gmail.com>
2025-03-13 21:19:23 +0800

    优化生词卡超出范围内容的滚动展示，修改卡片展开收起动画

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -16,34 +16,32 @@ const Card = ({ wordData, currIdx, totalNum }) => {
     >
       <div className="index">{currIdx + '/' + totalNum}</div>
       <div className="word">{word}</div>
-      {hovered &&
-        <>
-          <div className="pronunciation">
-            {pronounce.map((p, index) => (
-              <span key={index} className="pronunciation-item">
-                {p.region} {p.symbol}
-              </span>
-            ))}
-          </div>
-          <div className="explanations">
-            {explanations.map((exp, index) => (
-              <div key={index} className="explanation-item">
-                <span className="trait">{exp.trait}</span>
-                <span className="meaning">{exp.meaning}</span>
-                <span className="explain">{exp.explain}</span>
-              </div>
-            ))}
-          </div>
-          <div className="examples">
-            {example.map((ex, index) => (
-              <div key={index} className="example-item">
-                <span className="exampleEn">{ex.exampleEn}</span>
-                <span className="exampleZh">{ex.exampleZh}</span>
-              </div>
-            ))}
-          </div>
-        </>
-      }
+      <div className={hovered ? 'card-content' : 'card-content-hidden'}>
+        <div className="pronunciation">
+          {pronounce.map((p, index) => (
+            <span key={index} className="pronunciation-item">
+              {p.region} {p.symbol}
+            </span>
+          ))}
+        </div>
+        <div className="explanations">
+          {explanations.map((exp, index) => (
+            <div key={index} className="explanation-item">
+              <span className="trait">{exp.trait}</span>
+              <span className="meaning">{exp.meaning}</span>
+              <span className="explain">{exp.explain}</span>
+            </div>
+          ))}
+        </div>
+        <div className="examples">
+          {example.map((ex, index) => (
+            <div key={index} className="example-item">
+              <span className="exampleEn">{ex.exampleEn}</span>
+              <span className="exampleZh">{ex.exampleZh}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
