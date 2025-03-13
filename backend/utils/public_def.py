984e4e191dd0cb527395b3c8251a1805d6cc7ef1
hurui <hu.rui0530@gmail.com>
2025-03-13 22:50:39 +0800

    消息格式重构

--- backend/utils/public_def.py
+++ backend/utils/public_def.py
@@ -2,3 +2,23 @@ from collections import namedtuple
 
 ITEM = namedtuple('Item', ['id', 'text', 'source', 'target', 'service', 'result', 'timestamp'])
 RESULT = namedtuple('Result', ['text', 'region', 'symbol', 'trait', 'meaning', 'explain', 'exampleZh', 'exampleEn'])
+
+RESULT_2 = namedtuple(
+    'Result2',
+    [
+        'text', # str
+        'region', # list
+        'symbol', # list
+        'meaning', # List[MEANING]
+    ]
+)
+MEANING = namedtuple(
+    'MEANING',
+    [
+        'mean', # str
+        'trait', # str
+        'explain', # str
+        'exampleEn', # list
+        'exampleZh', # list
+    ]
+)
